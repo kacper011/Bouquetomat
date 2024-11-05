dd2fd6d898c76e17ce979d0a602b60a4515780a0
Kacper Szabat <kacper.szabat99@gmail.com>
2024-11-05 17:35:31 +0100

    Creatin a notification entity

---
+++ bouquetomat/src/main/java/com/example/bouquetomat/model/Notification.java
@@ -0,0 +1,31 @@
+package com.example.bouquetomat.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    private String message;
+
+    private LocalDateTime sentDate;
+
+    private String notificationType; //"EMAIL" / "SMS"
+}
