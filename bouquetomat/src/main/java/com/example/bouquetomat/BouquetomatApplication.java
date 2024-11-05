02ab43351fba9f1b54c030e2d4acca2cbd0245ef
Kacper Szabat <kacper.szabat99@gmail.com>
2024-11-05 17:21:48 +0100

    First commit

---
+++ bouquetomat/src/main/java/com/example/bouquetomat/BouquetomatApplication.java
@@ -0,0 +1,13 @@
+package com.example.bouquetomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouquetomatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouquetomatApplication.class, args);
+	}
+
+}
---
+++ bouquetomat/src/test/java/com/example/bouquetomat/BouquetomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bouquetomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouquetomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
