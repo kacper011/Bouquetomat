ea21017e5bd2f3c45ee26dee81d6fee68230201d
Kacper Szabat <kacper.szabat99@gmail.com>
2024-11-05 17:27:16 +0100

    Creating a bouquet entity

---
+++ bouquetomat/src/main/java/com/example/bouquetomat/model/Bouquet.java
@@ -0,0 +1,26 @@
+package com.example.bouquetomat.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Bouquet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer number;
+
+    private Double price;
+
+    private Boolean isAvailable;
+}
