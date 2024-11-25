0763af6054c16e92f50a0a0e6f72fd167b7da07a
Kacper Szabat <kacper.szabat99@gmail.com>
2024-11-25 20:47:42 +0100

    Writing a test for Buy Bouquet Successful Purchase

--- bouquetomat/src/main/java/com/example/bouquetomat/exception/GlobalExceptionHandler.java
+++ bouquetomat/src/main/java/com/example/bouquetomat/exception/GlobalExceptionHandler.java
@@ -2,12 +2,8 @@ package com.example.bouquetomat.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
---
+++ bouquetomat/src/test/java/com/example/bouquetomat/service/BouquetServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.bouquetomat.service;
+
+import com.example.bouquetomat.model.Bouquet;
+import com.example.bouquetomat.model.BouquetOrder;
+import com.example.bouquetomat.model.BouquetStatus;
+import com.example.bouquetomat.repository.BouquetRepository;
+import com.example.bouquetomat.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BouquetServiceTest {
+
+    @Mock
+    private BouquetRepository bouquetRepository;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private BouquetService bouquetService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+
+    @DisplayName("Buy Bouquet Successful Purchase")
+    @Test
+    void testBuyBouquetSuccessfulPurchase() {
+
+        //Given
+        Long bouquetId = 1L;
+        Bouquet bouquet = new Bouquet();
+        bouquet.setId(bouquetId);
+        bouquet.setPrice(100.0);
+        bouquet.setIsAvailable(true);
+
+        when(bouquetRepository.findById(bouquetId)).thenReturn(Optional.of(bouquet));
+
+        //When
+        String result = bouquetService.buyBouquet(bouquetId);
+
+        //Then
+        assertEquals("Bukiet z numerem 1 został zakupiony!", result);
+        assertFalse(bouquet.getIsAvailable());
+        assertEquals(BouquetStatus.SOLD, bouquet.getStatus());
+        verify(bouquetRepository, times(1)).save(bouquet);
+        verify(orderRepository, times(1)).save(any(BouquetOrder.class));
+        verify(notificationService, times(1)).sendNotification(any(BouquetOrder.class));
+    }
+
+
+}
