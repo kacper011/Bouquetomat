06c9829d9952db097bced09a98f1b3a179baa6d6
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-04 17:50:15 +0100

    Add application config (name, version, owner)

--- bouquetomat/src/main/java/com/example/bouquetomat/BouquetomatApplication.java
+++ bouquetomat/src/main/java/com/example/bouquetomat/BouquetomatApplication.java
@@ -1,13 +1,23 @@
 package com.example.bouquetomat;
 
+import com.example.bouquetomat.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BouquetomatApplication {
+public class BouquetomatApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BouquetomatApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
